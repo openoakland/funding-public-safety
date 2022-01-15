4f8155f0a59ebb529295ef0573525c6876e0f9fe
Jess Sand <32755410+theecrit@users.noreply.github.com>
2022-01-14 18:45:55 -0800

    Configure passthrough paths

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,9 @@
 module.exports = function (eleventyConfig) {
 
 
-  eleventyConfig.addWatchTarget("src/css");
-  eleventyConfig.addPassthroughCopy("src/css"); // - define css folder -
-  eleventyConfig.addPassthroughCopy("src/assets/images"); // - define assets folder -
+  eleventyConfig.addWatchTarget("css");
+  eleventyConfig.addPassthroughCopy("css"); // - define css folder -
+  eleventyConfig.addPassthroughCopy("assets"); // - define assets folder -
 
   // - input/output customization -
   return {
