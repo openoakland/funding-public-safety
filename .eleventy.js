86460218442be8d103f0d698617b6a725ef50c3c
Jess Sand <32755410+theecrit@users.noreply.github.com>
2022-01-14 18:05:51 -0800

    Adjust pathPrefix location in config file

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,8 @@
 module.exports = function (eleventyConfig) {
 
 
-  eleventyConfig.addWatchTarget("src/css/");
-  eleventyConfig.addPassthroughCopy("src/css/"); // - define css folder -
+  eleventyConfig.addWatchTarget("src/css");
+  eleventyConfig.addPassthroughCopy("src/css"); // - define css folder -
   eleventyConfig.addPassthroughCopy("src/assets/images"); // - define assets folder -
 
   // - input/output customization -
@@ -10,8 +10,8 @@ module.exports = function (eleventyConfig) {
     dir: {
       input: "src",
       output: "docs",
-    },
     pathPrefix: "/funding-public-safety/", // - Append URL with GitHub Pages prefix -
+    },
   };
 
 
