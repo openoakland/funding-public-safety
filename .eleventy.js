927bb11e5a253ee9e4925d09bc75375ea42bd912
aemann2 <aemann2@gmail.com>
2022-01-17 14:37:04 -0800

    added hot-reloading for stylesheet changes

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,15 @@
 module.exports = function (eleventyConfig) {
+	eleventyConfig.addWatchTarget('./src/css');
+	eleventyConfig.addPassthroughCopy('./src/css'); // Copy css folder to output directory.
+	eleventyConfig.addPassthroughCopy('assets'); // Copy assets folder to output directory.
 
+	// - input/output customization -
+	return {
+		pathPrefix: '/funding-public-safety/', // Append URL with repo prefix for GH Pages.
 
-  eleventyConfig.addWatchTarget("css");
-  eleventyConfig.addPassthroughCopy("css"); // Copy css folder to output directory.
-  eleventyConfig.addPassthroughCopy("assets"); // Copy assets folder to output directory.
-
-  // - input/output customization -
-  return {
-
-    pathPrefix: "/funding-public-safety/", // Append URL with repo prefix for GH Pages.
-
-    dir: {
-      input: "src", // Tells 11ty to use /src folder
-      output: "docs", // Tells 11ty to build pages into the /docs folder.
-    },
-
-  };
-
-
-
+		dir: {
+			input: 'src', // Tells 11ty to use /src folder
+			output: 'docs', // Tells 11ty to build pages into the /docs folder.
+		},
+	};
 };
