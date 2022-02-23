1b4a8f871ed1d1ca226b50ebd99053cc2adc992a
Adam Mann <68879246+aemann2@users.noreply.github.com>
2022-02-23 15:43:33 -0800

    Fixes/remove docs (#103)
    * added items to .gitignore
    
    * removed /docs from tracking
    
    * added correct path for assets folder passthrough copy

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addWatchTarget('./src/css');
 	eleventyConfig.addPassthroughCopy('./src/css'); // Copy css folder to output directory.
-	eleventyConfig.addPassthroughCopy('assets'); // Copy assets folder to output directory.
+	eleventyConfig.addPassthroughCopy('./src/assets'); // Copy assets folder to output directory.
 
 	// Given a path that is relative to the *root* directory, prepend "../" as
 	// needed to construct a working URL based on the current page's URL.
@@ -9,8 +9,8 @@ module.exports = function (eleventyConfig) {
 	// - If the current page is at the site's root, the method will return 'assets/pic.jpg'.
 	// - If the current page is nested two levels deep (e.g. a/b/my_page.html),
 	//   the filter will return '../../assets/pic.jpg'.
-	eleventyConfig.addFilter("relativeUrl", function (relativePath) {
-	  var nestLevel = this.ctx.page.url.split('/').length - 1;
+	eleventyConfig.addFilter('relativeUrl', function (relativePath) {
+		var nestLevel = this.ctx.page.url.split('/').length - 1;
 		return Array(nestLevel).join('../') + relativePath;
 	});
 
